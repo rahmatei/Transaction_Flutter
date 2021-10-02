0473443986ab265f0489d9efa9ace1804821788d
unknown <Administrator@HRahmati-1376.city-bank.org>
2021-10-02 08:27:17 +0330

    bottom sheet

--- lib/add_Transaction.dart
+++ lib/add_Transaction.dart
@@ -4,10 +4,20 @@ class AddTransaction extends StatelessWidget {
   final Function addTrns;
   AddTransaction({required this.addTrns});
 
+  final TextEditingController _title = TextEditingController();
+  final TextEditingController _amount = TextEditingController();
+
+  void submitedData() {
+    String strTitle = _title.text;
+    double amount = double.parse(_amount.text);
+    if (strTitle.isEmpty || amount <= 0) {
+      return;
+    }
+    addTrns(strTitle, amount);
+  }
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController _title = TextEditingController();
-    TextEditingController _amount = TextEditingController();
     return Card(
       elevation: 5,
       child: Container(
@@ -20,6 +30,7 @@ class AddTransaction extends StatelessWidget {
                 labelText: 'Title',
               ),
               controller: _title,
+              onSubmitted: (_) => submitedData,
             ),
             TextField(
               decoration: InputDecoration(
@@ -27,11 +38,12 @@ class AddTransaction extends StatelessWidget {
               ),
               controller: _amount,
               keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitedData(),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextButton(
-                  onPressed: ()=>addTrns(_title.text,double.parse(_amount.text)),// addTrns,
+                  onPressed: () => submitedData(), // addTrns,
                   child: Text(
                     'Add Transaction',
                     style: TextStyle(color: Colors.purple),
--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,70 @@
+import 'package:app/add_Transaction.dart';
 import 'package:flutter/material.dart';
-import 'user_transaction.dart';
-
+import 'transaction_list.dart';
+import 'transaction.dart';
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Transaction> trns = [
+    Transaction('1', 'Shoes', 10.2, DateTime.now()),
+    Transaction('2', 'umberela', 13, DateTime.now()),
+    Transaction('3', 'gift', 20, DateTime.now())
+  ];
+  void addNewTransaction( String title , double amount) {
+    Transaction tr = new Transaction(DateTime.now().toString(), title, amount, DateTime.now());
+    setState(() {
+          trns.add(tr);
+    });
+  }
+  showbottom(BuildContext ctx) {
+    return showBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return AddTransaction(addTrns: addNewTransaction);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
         home: Scaffold(
-            appBar: AppBar(
-              title: Text('App Transaction'),
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                    width: double.infinity,
-                    child: Card(
-                      elevation: 5,
-                      color: Colors.blueAccent,
-                      child: Text('App Center'),
-                    ),
+          appBar: AppBar(
+            title: Text('App Transaction'),
+            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 5,
+                    color: Colors.blueAccent,
+                    child: Text('App Center'),
                   ),
-                  UserTransaction()
-                ],
-              ),
-            )));
+                ),
+                TransationList(transactionList: trns)
+              ],
+            ),
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+              onPressed: () {
+             showbottom(context);
+              },
+              child: Icon(Icons.add),
+              shape: BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(5))),
+        ));
   }
 }
