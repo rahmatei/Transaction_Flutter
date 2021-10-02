3d78ec3ce47254020617e263bc8a13a170373966
unknown <Administrator@HRahmati-1376.city-bank.org>
2021-10-02 08:33:17 +0330

    update bottom sheet

--- lib/transaction_list.dart
+++ lib/transaction_list.dart
@@ -12,44 +12,43 @@ class TransationList extends StatelessWidget {
         height: 300,
         child: ListView.builder(
           itemBuilder: (ctx, index) {
-            return 
-Container(
-                padding: EdgeInsets.all(5),
-                child: Card(
-                  elevation: 5,
-                  color: Colors.white,
-                  child: Row(
-                    children: [
-                      Container(
-                        width: 55,
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.all(5),
-                        margin: EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.blue, width: 3)),
-                        child: Text(
-                          '\$${transactionList[index].amount}',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
+            return Container(
+              padding: EdgeInsets.all(5),
+              child: Card(
+                elevation: 5,
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 55,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.blue, width: 3)),
+                      child: Text(
+                        '\$${transactionList[index].amount}',
+                        style: TextStyle(fontWeight: FontWeight.bold),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '${transactionList[index].title}',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 15),
-                          ),
-                          Text(
-                            '${transactionList[index].date.toString()}',
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${transactionList[index].title}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        ),
+                        Text(
+                          '${transactionList[index].date.toString()}',
+                          style: TextStyle(color: Colors.grey),
+                        )
+                      ],
+                    )
+                  ],
                 ),
-                );
+              ),
+            );
           },
           itemCount: transactionList.length,
         ));
